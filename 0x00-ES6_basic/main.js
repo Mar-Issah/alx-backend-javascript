9be35daa12c548e8473660a710055374af79fc6d
Mar-Issah <masy370@gmail.com>
2023-11-28 13:54:20 -0600

    Feat: add new files 10-loops.js and 11-createEmployee.js

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -2,5 +2,6 @@ import getFullBudgetObject from './9-getFullBudget.js';
 
 const fullBudget = getFullBudgetObject(20, 50, 10);
 
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
-console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+console.log(fullBudget);
+// console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+// console.log(fullBudget.getIncomeInEuros(fullBudget.income));
