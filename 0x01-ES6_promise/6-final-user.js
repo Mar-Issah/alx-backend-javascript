c511b202e1a2c5e2fe7253b6e5f65b1e08490d66
Mar-Issah <masy370@gmail.com>
2023-11-29 14:29:17 -0600

    Feat: add new file 7-load_balancer.js.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((res) => res.map((obj) => ({
+  const promiseArray = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promiseArray).then((res) => res.map((obj) => ({
     status: obj.status,
     value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
   })));
