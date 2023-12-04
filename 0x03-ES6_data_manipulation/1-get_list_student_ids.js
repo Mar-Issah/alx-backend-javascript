1b9279abf681772c2ce119194a1795dd0a3f8552
Mar-Issah <masy370@gmail.com>
2023-12-04 16:29:34 -0600

    Feat: implement function getStudentsByLocation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -7,7 +7,6 @@
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
     return students.map((student) => student.id);
-  } else {
-    return [];
   }
+  return [];
 }
---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves students based on given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - List of students.
+ * @param {String} city - The city.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
+export default function getStudentsByLocation(students, city) {
+  if (students instanceof Array) {
+    return students.filter((student) => student.location === city);
+  }
+  return [];
+}
