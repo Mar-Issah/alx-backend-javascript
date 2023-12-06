c8330a1fb08533f7722bfd05f6409309c93996aa
Mar-Issah <masy370@gmail.com>
2023-12-06 14:01:29 -0600

    Feat:implement code in task_1/js/main.ts add func printTeacher

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,13 +11,13 @@ export interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// export interface printTeacherFunction {
-//   (firstName: string, lastName: string): string;
-// }
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
 
-// export function printTeacher(firstName: string, lastName: string): string {
-//   return `${firstName[0]}. ${lastName}`;
-// }
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
 
 // export interface IStudentClassConstructor {
 //   new (firstName: string, lastName: string): IStudentClass;
