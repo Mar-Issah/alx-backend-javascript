ba56787f54f6c2af0786286254f991a2e2d96869
Mar-Issah <masy370@gmail.com>
2024-02-01 16:44:16 -0600

    Chore: remove unwanted code

--- 0x06-unittests_in_js/index.js
+++
@@ -1,4 +0,0 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
-console.log(calculateNumber(2.0, 3.8));
-console.log(calculateNumber(2.6, 3.9));
-console.log(calculateNumber(4.5, 3.5));
